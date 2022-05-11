5854a9d6abf4f24f02821ce788176d7a22443ef4
Ariel <70619259+Ariel2783@users.noreply.github.com>
2022-05-11 13:37:30 -0500

    Se arreglo el manifiesto para la nueva version del  compilador. Se actualizo la version target de android. Se creo el setGeter para el lado A de la clase DimRectangular.

--- app/src/main/java/net/refriglobal/hvactools/Operaciones/DimRectangular.java
+++ app/src/main/java/net/refriglobal/hvactools/Operaciones/DimRectangular.java
@@ -12,15 +12,15 @@ public class DimRectangular
     public Double getLadoB() { return ladoB; }
     public void setLadoB(double lado_B) { ladoB = lado_B; }
 
+    private static double ladoA;
+    public Double getLadoA() { return ladoA; }
+    public void setLadoA(double lado_A) { ladoA = lado_A; }
+    //TODO: 20220508: CONTINUAR con el dimensionamiento desde el lado B.
+
     public DimRectangular()
     {
+        ladoA = -1;
         ladoB = -1;
-    }
-
-    public void dimensionLadoB(double ladoA)
-    {
-        Interpolaciones setGetInfo = new Interpolaciones();
-        double diaEqv = setGetInfo.getDiametroEqvFinal();
     }
 
     public void dimensionLadoBBiseccion(double ladoA)
@@ -29,12 +29,13 @@ public class DimRectangular
         double diaEqv = setGetInter.getDiametroEqvFinal();
         double valorFinal = 0;
         double valorInicial = 0;
-        int i = 0;
         final double error = 0.001;
 
         //Busqueda del intervalo.
+        int i = 0;
         while (true)
         {
+            //2009 ASHRAE Handbook Fundamentals, CHAPTER 21, pg - 21.7, eq - 25
             valorFinal = (1.30*Math.pow( (ladoA*(i+1) ), 0.625) / Math.pow( (ladoA+(i+1)) ,0.25)) - diaEqv;
 
             if (valorFinal > 0)
@@ -86,7 +87,5 @@ public class DimRectangular
                 break;
             }
         }
-
-
     }
 }
